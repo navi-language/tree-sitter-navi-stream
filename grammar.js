5487b61fb267deb4b4ae423f8f2da1b6d66224a6
Jason Lee <huacnlee@gmail.com>
2025-07-15 13:40:47 +0800

    Fix invalid ident regex.

--- grammar.js
+++ grammar.js
@@ -1362,7 +1362,7 @@ module.exports = grammar({
                 choice(
                   /[^xu]/,
                   /u[0-9a-fA-F]{4}/,
-                  /u{[0-9a-fA-F]+}/,
+                  /u\{[0-9a-fA-F]+\}/,
                   /x[0-9a-fA-F]{2}/,
                 ),
               ),
@@ -1380,7 +1380,7 @@ module.exports = grammar({
           choice(
             /[^xu]/,
             /u[0-9a-fA-F]{4}/,
-            /u{[0-9a-fA-F]+}/,
+            /u\{[0-9a-fA-F]+\}/,
             /x[0-9a-fA-F]{2}/,
           ),
         ),
