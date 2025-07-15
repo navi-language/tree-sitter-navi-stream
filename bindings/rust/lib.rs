5dc0f1b53d9a62b84af44e266808b20f5c0dfd17
Jason Lee <huacnlee@gmail.com>
2025-07-15 13:37:44 +0800

    Upgrade tree-sitter

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_navi_stream::language()).expect("Error loading navi_stream grammar");
+//! parser.set_language(&tree_sitter_navi_stream::LANGUAGE.into()).expect("Error loading navi_stream grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -14,19 +14,16 @@
 //! [language func]: fn.language.html
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
-
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_navi_stream() -> Language;
+    fn tree_sitter_navi_stream() -> *const ();
 }
 
 /// Get the tree-sitter [Language][] for this grammar.
 ///
 /// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_navi_stream() }
-}
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_navi_stream) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -35,10 +32,10 @@ pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
 
 // Uncomment these to include any queries that this grammar contains
 
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
+pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
 // pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
 // pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -46,7 +43,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(super::language())
+            .set_language(&super::LANGUAGE.into())
             .expect("Error loading navi_stream language");
     }
 }
